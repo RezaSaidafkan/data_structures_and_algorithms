baa167848918bceb73bd963d735a7791aab95cf2
Reza Saidafkan <reza.saidafkan@gmail.com>
2025-10-10 14:44:48 +0200

    add insert & search methods for HeapTree

---
+++ src/heap.spec.ts
@@ -0,0 +1,69 @@
+import {describe, it, expect} from 'vitest';
+import { HeapTree, HeapNode } from './heap';
+import {EMPTY_DATASTRUCTURE_MESSAGE, ELEMENT_NOT_FOUND_MESSAGE} from './dataStructureInterface'
+
+
+describe('head', () => {
+    describe('insert', () => {
+        it('should insert new values in order', () => {
+            // Arrange
+            const heapTree = new HeapTree();
+
+            // Act
+            heapTree.insert(1)
+            heapTree.insert(4)
+            heapTree.insert(3)
+            heapTree.insert(2)
+            heapTree.insert(5)
+            
+            // Assert
+            expect(heapTree).toEqual(expect.objectContaining(
+                {'heapArray': [
+                    expect.objectContaining({value: 1}),
+                    expect.objectContaining({value: 3}),
+                    expect.objectContaining({value: 2}),
+                    expect.objectContaining({value: 4}),
+                    expect.objectContaining({value: 5}),
+                    ]
+                })
+            )
+        })
+    })
+
+    describe('search', () => {
+        it('should throw if the heapTree is empty', () => {
+            // Arrange
+            const heapTree = new HeapTree();
+
+            // Act & Assert
+            expect(() => heapTree.search('some value')).toThrow(EMPTY_DATASTRUCTURE_MESSAGE)
+        })
+
+        it('should return the matching HeapNode in a single node array', () => {
+            // Arrange
+            const heapTree = new HeapTree();
+            heapTree.insert(1);
+
+            // Act & Assert
+            expect(heapTree.search(1)).toEqual(expect.objectContaining(
+                {value: 1}
+            ))
+        })
+        
+        it('should return the matching HeapNode in a multiple node array', () => {
+            // Arrange
+            const heapTree = new HeapTree();
+            heapTree.insert(1);
+            heapTree.insert(5);
+            heapTree.insert(4);
+            heapTree.insert(3);
+            heapTree.insert(2);
+            
+
+            // Act & Assert
+            expect(heapTree.search(5)).toEqual(expect.objectContaining(
+                {value: 5}
+            ))
+        })
+    })
+})
